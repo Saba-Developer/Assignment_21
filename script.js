573c525e56c32298274fad4b6051d66e7ed4f3af
Saba-Developer <sabamadina87@gmail.com>
2025-04-25 12:14:36 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let item=prompt("Enter the name of Item You are buying: ");
+let price=parseInt(prompt("Enter the price of your item: "));
+let quantity=parseInt(prompt("Enter the Quantity of your Item: "));
+console.log(`The Subtotal of your Item : ${quantity * price}`);
